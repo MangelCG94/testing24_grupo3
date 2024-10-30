b995c048280ea73d088b99069cd03b900ace2741
juanralvaro <juanralvaro@yahoo.es>
2024-10-30 09:36:18 +0100

    Clase Usuario

---
+++ src/main/java/com/games/games/models/Usuario.java
@@ -0,0 +1,31 @@
+package com.games.games.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombreUsuario;
+    private String password;
+    private String nombre;
+    private String direccion;
+    private Integer CP;
+    private String DNI;
+    private Date fechaCreacion;
+
+}
