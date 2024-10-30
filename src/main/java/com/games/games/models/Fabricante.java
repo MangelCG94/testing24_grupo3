b16a25aedc2daa4ae8ea251f05a9661aa321c9c3
IsmaelSierra <isg.mpp.6@gmail.com>
2024-10-30 11:27:43 +0100

    Clase Fabricante

--- src/main/java/com/games/games/models/Fabricante.java
+++ src/main/java/com/games/games/models/Fabricante.java
@@ -1,5 +1,22 @@
 package com.games.games.models;
 
-public class Fabricante {
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+
+public class Fabricante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombreCom;
+    private String pais;
+    private String imagenLogo;
+    private Integer anyoFundacion;
 }
