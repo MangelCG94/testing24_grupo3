32ed3f78ab4f4255daf9a350ddde132127ef39f2
juanralvaro <juanralvaro@yahoo.es>
2024-11-10 17:46:33 +0100

    #11 Repositorio Usuario (ligero cambio)

--- src/main/java/com/games/games/repositories/UsuarioRepository.java
+++ src/main/java/com/games/games/repositories/UsuarioRepository.java
@@ -3,6 +3,7 @@ package com.games.games.repositories;
 import com.games.games.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+import java.util.List;
 
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
