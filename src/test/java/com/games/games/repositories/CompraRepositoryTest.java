31b2328c8463a3c0418fb646b571bafc9ae4357f
juanralvaro <juanralvaro@yahoo.es>
2024-11-13 21:39:57 +0100

    #27 Test Repositorio Compra (corrección de errores)

--- src/test/java/com/games/games/repositories/CompraRepositoryTest.java
+++ src/test/java/com/games/games/repositories/CompraRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.games.games.repositories;
 
-import com.games.games.dtos.CompraConJuegosUsuario;
+import com.games.games.dtos.CompraConJuegosDTO;
 import com.games.games.models.Compra;
+import com.games.games.models.Juego;
 import com.games.games.models.JuegosUsuario;
+import com.games.games.models.Usuario;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,11 +12,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @DataJpaTest
 class CompraRepositoryTest {
@@ -23,16 +23,19 @@ class CompraRepositoryTest {
     private CompraRepository compraRepository;
 
     @Autowired
-    private JuegosUsuarioRepository juegosUsuarioRepository;
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private JuegoRepository juegoRepository;
 
     @Test
     void findByFechaCompra() {
-        Compra compra1 = Compra.builder().fechaCompra(10000000L).build();
-        Compra compra2 = Compra.builder().fechaCompra(20000000L).build();
+        Compra compra1 = Compra.builder().fechaCompra(Instant.now()).build();
+        Compra compra2 = Compra.builder().fechaCompra(Instant.ofEpochSecond(10000)).build();
 
         compraRepository.saveAll(List.of(compra1, compra2));
 
-        List<Compra> compras = compraRepository.findByFechaCompra(10000000L);
+        List<Compra> compras = compraRepository.findByFechaCompra(Instant.now());
 
         assertEquals(2, compras.size());
         assertTrue(compras.contains(compra1));
@@ -41,19 +44,19 @@ class CompraRepositoryTest {
     }
 
     @Test
-    void findByJuegosUsuario() {
+    void findByUsuarios() {
 
-        JuegosUsuario juegosUsuario1 = JuegosUsuario.builder().build();
-        JuegosUsuario juegosUsuario2 = JuegosUsuario.builder().build();
+        Usuario usuario1 = Usuario.builder().build();
+        Usuario usuario2 = Usuario.builder().build();
 
-        juegosUsuarioRepository.saveAll(List.of(juegosUsuario1, juegosUsuario2));
+        usuarioRepository.saveAll(List.of(usuario1, usuario2));
 
-        Compra compra1 = Compra.builder().juegosUsuario(juegosUsuario1).build();
-        Compra compra2 = Compra.builder().juegosUsuario(juegosUsuario2).build();
+        Compra compra1 = Compra.builder().usuario(usuario1).build();
+        Compra compra2 = Compra.builder().usuario(usuario1).build();
 
         compraRepository.saveAll(List.of(compra1, compra2));
 
-        List<Compra> compras = compraRepository.findByJuegosUsuario(juegosUsuario1);
+        List<Compra> compras = compraRepository.findByUsuarios(usuario1);
 
         assertEquals(1, compras.size());
         assertTrue(compras.contains(compra1));
@@ -61,42 +64,62 @@ class CompraRepositoryTest {
     }
 
     @Test
-    void encuentraTodasLasComprasConJuegosDeUsuario() {
+    void findByJuegos() {
+
+        Juego juego1 = Juego.builder().build();
+        Juego juego2 = Juego.builder().build();
+
+        juegoRepository.saveAll(List.of(juego1, juego2));
+
+        Compra compra1 = Compra.builder().juego(juego1).build();
+        Compra compra2 = Compra.builder().juego(juego2).build();
+
+        compraRepository.saveAll(List.of(compra1, compra2));
+
+        List<Compra> compras = compraRepository.findByJuegos(juego1);
+
+        assertEquals(1, compras.size());
+        assertTrue(compras.contains(compra1));
+        assertFalse(compras.contains(compra2));
+    }
+
+    @Test
+    void encuentraTodasLasComprasConJuegos() {
+
+        Juego juego1 = Juego.builder()
+                .id(1L)
+                .nombre("The legend of Zelda")
+                .build();
+
+        Juego juego2 = Juego.builder()
+                .id(2L)
+                .nombre("FIFA 14")
+                .build();
+
+        juegoRepository.saveAll(List.of(juego1, juego2));
 
         Compra compra1 = Compra.builder()
-                .fechaCompra(LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli())
-                .juegosUsuario(JuegosUsuario.builder().build())
+                .fechaCompra(Instant.ofEpochSecond(LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli()))
+                .juego(juego1)
                 .build();
 
         compraRepository.save(compra1);
 
-        JuegosUsuario juegosUsuario1 = JuegosUsuario.builder()
-                .id(1L)
-                .time(150L)
-                .build();
-
-        JuegosUsuario juegosUsuario2 = JuegosUsuario.builder()
-                .id(2L)
-                .time(100L)
-                .build();
-
-        juegosUsuarioRepository.saveAll(List.of(juegosUsuario1, juegosUsuario2));
-
-        List<CompraConJuegosUsuario> compras = compraRepository.encuentraTodasLasComprasConJuegosDeUsuario(1L, Instant.now());
+        List<CompraConJuegosDTO> compras = compraRepository.encuentraTodasLasComprasConJuegos(1L, Instant.now());
 
         assertNotNull(compras);
         assertEquals(1, compras.size());
-        CompraConJuegosUsuario dto = compras.get(0);
+        CompraConJuegosDTO dto = compras.get(0);
         assertEquals(compra1.getIdCompra(), dto.compraId());
     }
 
     @Test
     void finByFechaCompraEntre() {
         Compra compra1 = Compra.builder()
-                .fechaCompra(Instant.now().minusSeconds(10000).toEpochMilli())
+                .fechaCompra(Instant.ofEpochSecond(Instant.now().minusSeconds(10000).toEpochMilli()))
                 .build();
         Compra compra2 = Compra.builder()
-                .fechaCompra(Instant.now().minusSeconds(5000).toEpochMilli())
+                .fechaCompra(Instant.ofEpochSecond(Instant.now().minusSeconds(5000).toEpochMilli()))
                 .build();
 
         compraRepository.saveAll(List.of(compra1, compra2));
