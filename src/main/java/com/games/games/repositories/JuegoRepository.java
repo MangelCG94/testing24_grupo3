bee3aed93ef3596707f4ea75e1c20dae8af73bd8
MangelCG94 <mangelc13@gmail.com>
2024-11-19 09:14:17 +0100

    Fix para fecha de juegos

--- src/main/java/com/games/games/repositories/JuegoRepository.java
+++ src/main/java/com/games/games/repositories/JuegoRepository.java
@@ -2,6 +2,21 @@ package com.games.games.repositories;
 
 import com.games.games.models.Juego;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface JuegoRepository extends JpaRepository<Juego, Long> {
+    List<Juego> findByDesarrolladoraNombreCom(String nombreCom);
+
+    List<Juego> findByPrecioLessThan(Double precio); //LessThan = MenosQue
+
+    @Query("SELECT j FROM Juego j WHERE j.desarrolladora.pais = :pais")
+    List<Juego> findByDesarrolladoraPais(@Param("pais") String pais);
+
+    @Query("SELECT j FROM Juego j " +
+            "WHERE j.precio < :maxPrice " +
+            "AND j.desarrolladora.anyoFundacion > :year")
+    List<Juego> findByPriceAndFoundationYear(@Param("maxPrice") Double maxPrice, @Param("year") Integer year);
 }
