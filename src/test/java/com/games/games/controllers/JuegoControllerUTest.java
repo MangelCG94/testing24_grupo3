7b11593f6f5faadc16e073c94f45f67d41b0ed99
MangelCG94 <mangelc13@gmail.com>
2024-12-09 11:33:57 +0100

    Arreglo excepcion JuegoController.

--- src/test/java/com/games/games/controllers/JuegoControllerUTest.java
+++ src/test/java/com/games/games/controllers/JuegoControllerUTest.java
@@ -1,7 +1,9 @@
 package com.games.games.controllers;
 
+import com.games.games.models.Desarrolladora;
 import com.games.games.models.Juego;
 import com.games.games.repositories.JuegoRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,10 +12,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.Model;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class JuegoControllerUTest {
@@ -25,6 +27,7 @@ public class JuegoControllerUTest {
     private Model model;
 
     @Test
+    @DisplayName("Encontrar una lista de juegos")
     void findAllTest() {
         when(repository.findAll()).thenReturn(List.of(
                 Juego.builder().build(),
@@ -36,5 +39,65 @@ public class JuegoControllerUTest {
         verify(repository).findAll();
     }
 
+    @Test
+    void findByIdWhenJuegoExists() {
+        Juego juego = Juego.builder().id(1L).desarrolladora(Desarrolladora.builder().nombreCom("empresa").build()).build();
+        when(repository.findById(1L)).thenReturn(Optional.of(juego));
 
+        String view = controller.findById(1L, model);
+
+        assertEquals("juego-detail", view);
+        verify(repository).findById(1L);
+        verify(repository, never()).findAll();
+        verify(model).addAttribute("juego", juego);
+    }
+
+    @Test
+    void findByIdWhenJuegoNotExists() {
+        when(repository.findById(1L)).thenReturn(Optional.empty());
+
+        String view = controller.findById(1L, model);
+
+        assertEquals("juego-detail", view);
+        verify(repository).findById(1L);
+        verify(model,never()).addAttribute(anyString(), any());
+    }
+
+    @Test
+    void createJuegoForm() {
+        String view = controller.formCreate(model);
+
+        assertEquals("juego-form", view);
+        verify(model).addAttribute(eq("juego"), Juego.class);
+    }
+
+    @Test
+    @DisplayName("Formulario para actualizar una compra si ésta existe")
+    void formularioParaActualizarCompraSiExiste() {
+        Juego juego = Juego.builder().id(1L).desarrolladora(Desarrolladora.builder().build()).build();
+        when(repository.findById(1L)).thenReturn(Optional.of(juego));
+
+        String view = controller.formUpdate(model, 1L);
+
+        assertEquals("juego-form", view);
+        verify(repository).findById(1L);
+        verify(model).addAttribute("juego", juego);
+    }
+
+    @Test
+    @DisplayName("Formulario para actualizar una compra si ésta no existe")
+    void formularioParaActualizarCompraSiNoExiste() {
+        when(repository.findById(1L)).thenReturn(Optional.empty());
+
+        String view = controller.formUpdate(model, 1L);
+
+        assertEquals("error", view);
+        verify(repository).findById(1L);
+        verify(model).addAttribute(eq("mensaje"), eq("Compra no encontrada"));
+
+        verify(model, never()).addAttribute(eq("compra"), any());
+        verify(model, never()).addAttribute(eq("juegos"), any());
+        verify(model, never()).addAttribute(eq("usuarios"), any());
+
+    }
 }
